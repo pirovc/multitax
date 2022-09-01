75c6a76058eb7835bab653a2225facaf1ba2c3eb
pirovc <4673375+pirovc@users.noreply.github.com>
2022-09-01 15:44:11 +0200

    Feature/infra (#6)
    * update setup.py and coverage
    
    * setuptools
    
    * setuptools upgrade
    
    * importlib-metadata
    
    * fix docs

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
+import io
+import os
+import re
+
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
 
 setup(
     name="multitax",
     version="1.2.0",
     url="https://www.github.com/pirovc/multitax",
-    license='MIT',
-
+    license="MIT",
     author="Vitor C. Piro",
-    author_email="pirovc@posteo.net",
-
     description="Python package to obtain, parse and explore biological and custom taxonomies",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-
-    packages=['multitax'],
-
+    long_description=read("README.md"),
+    packages=["multitax"],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
-    python_requires=">=3.4",
 )
