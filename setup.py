61c2b9872011e9b9581ced53cdaeec98c6252e7c
pirovc <pirovc@posteo.net>
2025-07-09 19:30:13 +0200

    update python versions setup

--- setup.py
+++ setup.py
@@ -20,11 +20,10 @@ setup(
     python_requires=">=3.4",
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 )
