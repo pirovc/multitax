ff67fc6f9cfe15165f3da0c8cc31b18b6e8e5c13
pirovc <4673375+pirovc@users.noreply.github.com>
2026-03-28 19:12:54 +0100

    Feature/integrate lca (#28)
    * LCA integrated
    
    * Add dependency on pylca
    
    * docs

--- src/multitax/multitax.py
+++ src/multitax/multitax.py
@@ -11,6 +11,7 @@ from multitax.utils import (
 )
 from collections import Counter
 from datetime import datetime
+from pylca.pylca import LCA
 
 
 class MultiTax(object):
@@ -87,6 +88,7 @@ class MultiTax(object):
         self._node_children = {}
         self._rank_nodes = {}
         self._translated_nodes = {}
+        self._lca = None
 
         # Properties
         self.datetime = datetime.now()
@@ -205,6 +207,7 @@ class MultiTax(object):
         self._node_children = {}
         self._rank_nodes = {}
         self._translated_nodes = {}
+        self._lca = None
 
     def _set_root_node(self, root: str, parent: str, name: str, rank: str):
         """
@@ -255,8 +258,8 @@ class MultiTax(object):
 
     def add(self, node: str, parent: str, name: str = None, rank: str = None):
         """
-        Add node to taxonomy.
-        Deletes built lineages and translations.
+        Adds node to taxonomy.
+        Deletes built lineages, translations and lca.
         """
         if parent not in self._nodes:
             raise ValueError("Parent node [" + parent + "] not found.")
@@ -267,6 +270,15 @@ class MultiTax(object):
         self._names[node] = name if name is not None else self.undefined_name
         self._ranks[node] = rank if rank is not None else self.undefined_rank
         self._reset_aux_data()
+
+    def build_lca(self):
+        """
+        Builds LCA structure based on pylca.
+        Optional function, LCA is built on first .lca() call.
+
+        Returns: None
+        """
+        self._lca = LCA(self._nodes)
 
     def build_lineages(self, root_node: str = None, ranks: list = None):
         """
@@ -359,6 +371,14 @@ class MultiTax(object):
 
         return None
 
+    def clear_lca(self):
+        """
+        Clear built LCA.
+
+        Returns: None
+        """
+        self._lca = None
+
     def clear_lineages(self):
         """
         Clear built lineages.
@@ -387,7 +407,7 @@ class MultiTax(object):
         Filters taxonomy given a list of nodes.
         By default keep all the ancestors of the given nodes.
         If desc=True, keep all descendants instead.
-        Deletes built lineages and translations.
+        Deletes built lineages, translations and lca.
 
         Example:
 
@@ -482,6 +502,26 @@ class MultiTax(object):
         else:
             return []
 
+    def lca(self, nodes: list = None):
+        """
+        Returns the lowest common ancestor of two or more nodes.
+
+        Example:
+
+            from multitax import GtdbTx
+            tax = GtdbTx()
+            tax.lca(["s__Escherichia coli", "s__Escherichia fergusonii"])
+        """
+        for node in nodes:
+            if node not in self._nodes:
+                raise ValueError("Node [" + node + "] not found.")
+
+        # Setup on first use
+        if not self._lca:
+            self.build_lca()
+
+        return self._lca(*nodes)
+
     def lineage(self, node: str, root_node: str = None, ranks: list = None):
         """
         Returns a list with the lineage of a given node.
@@ -575,7 +615,7 @@ class MultiTax(object):
     def prune(self, nodes: list):
         """
         Prunes branches of the tree under the given nodes.
-        Deletes built lineages and translations.
+        Deletes built lineages, translations and lca.
         """
 
         if isinstance(nodes, str):
@@ -615,7 +655,7 @@ class MultiTax(object):
         """
         Removes node from taxonomy. Can break the tree if a parent node is removed. To remove a certain branch, use prune.
         Running check consistency after removing a node is recommended.
-        Deletes built lineages and translations.
+        Deletes built lineages, translations and lca.
         """
         if node not in self._nodes:
             raise ValueError("Node [" + node + "] not found.")
