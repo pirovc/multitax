853e7b897d78c3c73bf8c2fd971bbd6b8d3293ad
pirovc <4673375+pirovc@users.noreply.github.com>
2026-04-15 17:32:58 +0200

    Apply suggestion from @pirovc

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,6 +6,6 @@ ruff check --fix
 echo "Unit tests"
 python -m unittest discover -s tests/multitax/unit/
 echo "Integration tests"
-python -m unittest discover -s tests/multitax/integration/ -v
+python -m unittest discover -s tests/multitax/integration/
 
 pdoc -o docs multitax multitax.multitax multitax.utils
